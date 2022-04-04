891095e0522f9094fb8f4193ae8d6687eb605343
always0ne <always0ne.dev@gmail.com>
2022-04-05 00:28:02 +0900

    init project

---
+++ src/test/kotlin/com/ssu/commerce/core/CoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ssu.commerce.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
