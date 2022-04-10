7869cbba93f4dd1f49f5ee21ab21fc80c5d4b560
always0ne <always0ne.dev@gmail.com>
2022-04-10 19:38:30 +0900

    성공했을때만 배포되도록 수정(의도적으로 빌드실패되게함)

--- src/main/kotlin/com/ssu/commerce/core/security/JwtTokenProvider.kt
+++ src/main/kotlin/com/ssu/commerce/core/security/JwtTokenProvider.kt
@@ -10,7 +10,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.stereotype.Component
-import java.util.Date
+import java.util.*
 import javax.crypto.SecretKey
 import javax.servlet.http.HttpServletRequest
 
