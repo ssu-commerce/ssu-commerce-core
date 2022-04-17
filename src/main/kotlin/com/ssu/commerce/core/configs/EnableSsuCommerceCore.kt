deaac148ce963c6e5e932b2a757e6316e7df192e
always0ne <always0ne.dev@gmail.com>
2022-04-17 21:11:34 +0900

    어노테이션 타겟 수정

--- src/main/kotlin/com/ssu/commerce/core/configs/EnableSsuCommerceCore.kt
+++ src/main/kotlin/com/ssu/commerce/core/configs/EnableSsuCommerceCore.kt
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
-@Target(AnnotationTarget.TYPE)
+@Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Import(EnableSsuCommerceCoreConfiguration::class)
 annotation class EnableSsuCommerceCore
