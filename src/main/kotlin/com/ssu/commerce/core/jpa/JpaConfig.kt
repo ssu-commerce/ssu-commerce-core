88e00413c8beb692870718f5867ec8abc0519323
naemoo <gusdn0657@gmail.com>
2022-05-21 16:50:52 +0900

    feat : JPA Base Entity 설정 및 테스트

---
+++ src/main/kotlin/com/ssu/commerce/core/jpa/JpaConfig.kt
@@ -0,0 +1,25 @@
+package com.ssu.commerce.core.jpa
+
+import com.ssu.commerce.core.exception.SsuCommerceException
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+    
+    @Bean
+    //TODO: Error code 설정
+    fun auditorProvider(): AuditorAware<String> =
+        AuditorAware {
+            Optional.of(
+                SecurityContextHolder.getContext().authentication?.name
+                    ?: throw SsuCommerceException(HttpStatus.FORBIDDEN, null, "Not exist authentication")
+            )
+        }
+}
