4dbac5d36c020da91b73894af636bbb7a5cb7fd5
always0ne <always0ne.dev@gmail.com>
2024-09-16 18:09:09 +0900

    webflux feign 세팅 추가

--- ssu-commerce-cores/ssu-commerce-core-io/src/main/kotlin/com/ssu/commerce/core/io/config/FeignConfig.kt
+++ ssu-commerce-cores/ssu-commerce-core-io/src/main/kotlin/com/ssu/commerce/core/io/config/FeignConfig.kt
@@ -1,8 +1,20 @@
 package com.ssu.commerce.core.io.config
 
+import org.springframework.beans.factory.ObjectProvider
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import java.util.stream.Collectors
 
 @Configuration
 @EnableFeignClients(basePackages = ["com.ssu.commerce"])
-class FeignConfig
+class FeignConfig {
+    @Bean
+    @ConditionalOnMissingBean
+    fun messageConverters(converters: ObjectProvider<HttpMessageConverter<*>?>): HttpMessageConverters {
+        return HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()))
+    }
+}
